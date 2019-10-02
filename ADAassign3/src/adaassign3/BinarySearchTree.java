cb4a0b624bd6e67fbd28fc4fd022c349fd0f5cce
WilsonLiu25 <32662781+WilsonLiu25@users.noreply.github.com>
2019-10-02 21:08:16 +1300

    Continuing Binary Search tree

--- ADAassign3/src/adaassign3/BinarySearchTree.java
+++ ADAassign3/src/adaassign3/BinarySearchTree.java
@@ -36,9 +36,100 @@ public class BinarySearchTree implements Dictionary {
     }
     
     //Traverse the tree in inorder applying a Visitor to each node
-    public void inorderWalk(Visitor visitor) {
+    public void inorderWalk(Visitor visitor){
         inorderWalk(root, visitor);
     }
+    
+    protected void inorderWalk(Node x, Visitor visitor) {
+        if (x != nil) {
+            inorderWalk(x.left, visitor);
+            visitor.visit(x);
+            inorderWalk(x.right, visitor);
+        }
+    }
+    
+    public void preorderWalk(Visitor visitor) {
+        preorderWalk(root, visitor);
+    }
+    
+    protected void preorderWalk(Node x, Visitor visitor) {
+        if (x != nil) {
+            visitor.visit(x);
+            preorderWalk(x.left,visitor);
+            preorderWalk(x.right,visitor);
+        }
+    }
+    
+    public void postorderWalk(Visitor visitor) {
+        postorderWalk(root, visitor);
+    }
+    
+    protected void postorderWalk(Node x, Visitor visitor){
+        if (x != nil) {
+            postorderWalk(x.left,visitor);
+            postorderWalk(x.left, visitor);
+            visitor.visit(x);
+        }
+    }
+    
+    //returns a multiline String representation of the tree, representing the depth
+    //of each node by two spaces per depth preceding the String representation of the node
+    public String toString(){
+        return root.toString(0);
+    }
+    
+    public Object search(Comparable k) {
+        return search(root, k);
+    }
+    
+    //Searches the subtree rooted at a given node for a node with a given key
+    protected Object search(Node x, Comparable k) {
+        int c;
+        
+        if (x == nil || (c = k.compareTo(x.data)) == 0) {
+            return x;
+        }
+        
+        if (c < 0) {
+            return search(x.left, k);
+        }else {
+            return search(x.right, k);
+        }
+    }
+    
+    //Searches the tree for a node with a given key
+    public Object iterativeSearch(Comparable k) {
+        Node x = root;
+        int c;
+        
+        while (x != nil && (c = k.compareTo(x.data)) != 0) {
+            if (c < 0) {
+                x = x.left; 
+            } else {
+                x = x.right;
+            }
+        }
+        
+        return x;
+    }
+    
+    //returns the node with the minimum key in the tree
+    public Object minimum() {
+        return treeMinimum(root);
+    }
+    
+    protected Object treeMinimum(Node x) {
+        while (x.left != nil) {
+            x = x.left;
+        }
+        
+        return x;
+    }
+    
+    
+    
+    
+    
     
     
     
